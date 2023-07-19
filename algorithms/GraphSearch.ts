801c3e48dc23394b52b335983b2155812e58a7b6
MenelaosI <menelaos.ioannou@gmail.com>
2023-07-19 17:12:27 -0400

    Adds in comments to make this prettier

---
+++ algorithms/GraphSearch.ts
@@ -0,0 +1,2 @@
+// TODO: Implement BFS and DFS
+
--- utils/LinkedList.ts
+++ utils/LinkedList.ts
@@ -1,20 +1,40 @@
+/**
+ * Class for a LinkedList Node with data of any type
+ */
 class LinkedListNode<T> {
     data: T;
     next: LinkedListNode<T> | null;
 
+    /**
+     * Creates a LinkedListNode and sets the next one to null
+     * 
+     * @param {T} data - The data for the LinkedListNode
+     */
     constructor(data: T) {
         this.data = data;
         this.next = null;
     }
 }
 
+/**
+ * Class for a LinkedList of any type T
+ */
 class LinkedList<T> {
     head: LinkedListNode<T> | null;
     
+    /**
+     * Constructs the LinkedList starting with a null head
+     */
     constructor() {
         this.head = null; 
     }
 
+    /**
+     * Function to append a node to the LinkedList
+     * 
+     * @param {T} data - Adds data to the LinkedList
+     * @returns 
+     */
     append(data: T) {
         const newNode = new LinkedListNode(data);
 

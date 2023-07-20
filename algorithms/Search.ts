925fa290d6bceb8b3b7ba452c88040ff7c52df53
MenelaosI <menelaos.ioannou@gmail.com>
2023-07-20 16:49:18 -0400

    Adds tilted array to search

--- algorithms/Search.ts
+++ algorithms/Search.ts
@@ -9,17 +9,43 @@
  */
 function binarySearch(array: number[], left: number, right: number, target: number): number {
     if(right >= left) {
-        let midpoint = Math.floor((left + right) / 2);
+        const midpoint = Math.floor((left + right) / 2);
 
-        if(array[midpoint] === target) { 
-            return midpoint;
-        }
+        if(array[midpoint] === target) return midpoint;
 
-        if(array[midpoint] > target) {
-            return binarySearch(array, left, midpoint - 1, target);
-        }
+        if(array[midpoint] > target) return binarySearch(array, left, midpoint - 1, target);
         
         return binarySearch(array, midpoint + 1, right, target);
     }
     return -1;
-}
+}; 
+
+/**
+ * Does a binary search on a tilted array
+ * 
+ * @param {number[]} array - The array of numbers to search
+ * @param {number} left - The left value in the array
+ * @param {number} right - The right value in the array
+ * @param {number} target - The number being searched for
+ * @returns the index of the number if found our -1
+ */
+function binarySearchTilted(array: number[], left: number, right: number, target: number): number {
+    if(left > right) return -1;
+    const midpoint = Math.floor((left + right) / 2);
+
+    if(array[midpoint] === target) return midpoint;
+
+    if(array[left] <= array[midpoint]) {
+        if(target >= array[left] && target <= array[midpoint]) {
+            return binarySearchTilted(array, left, midpoint - 1, target);
+        }
+
+        return binarySearchTilted(array, midpoint + 1, right, target);
+    }
+
+    if(target >= array[midpoint] && target <= array[right]) {
+        return binarySearchTilted(array, midpoint + 1, right, target);
+    }
+
+    return binarySearchTilted(array, left, midpoint - 1, target);
+};
